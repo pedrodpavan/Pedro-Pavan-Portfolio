2b0edd91771466c34ed57a40841313c046b3d29f
Pedro Pavan <106819988+pedrodpavan@users.noreply.github.com>
2024-10-14 21:18:52 -0300

    Adição da versão Mobile
    Deixei o site responsivo para dispositivos moveis

---
+++ script.js
@@ -0,0 +1,9 @@
+AOS.init();
+
+
+<script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
+<script type="text/javascript">
+    (function () {
+        emailjs.init("N8vvfhd7vHdNSYRVN");  // Substitua "YOUR_USER_ID" pelo seu User ID do EmailJS
+    })();
+</script>
